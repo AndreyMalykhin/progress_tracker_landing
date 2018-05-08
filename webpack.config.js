ae8ac0d80946d3cf27629fec63aabcc13b914283
AndreyMalykhin <shell100500@gmail.com>
2018-05-08 05:07:42 +0300

    Added page structure

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,10 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.svg$/,
+        loader: 'svg-inline-loader'
+      },
+      {
         test: /\.(png|jpg|gif)$/,
         use: [
           {
